be62eef7db740fceeba59f28ec7c35346276841f
hikaruasamura <qiancunhui@gmail.com>
2023-11-25 18:09:16 +0900

    migrate

--- database/migrations/2013_11_25_165828_create_songs_table.php
+++ database/migrations/2013_11_25_165828_create_songs_table.php
@@ -13,16 +13,10 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('songs', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
-            $table->time('awaketime');
-            $table->foreignId('song_id')->constrained();
+            $table->string('song');
         });
     }
 
@@ -33,6 +27,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('songs');
     }
 };
